c56018d16ebd25aca1341195056cadd881bfdfeb
Nam Tran Trong <nam.tran-trong@ubisoft.com>
2023-09-16 14:56:15 +0700

    add tabbae

---
+++ app/src/main/java/com/pbl/app_mobile/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.pbl.app_mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+}
