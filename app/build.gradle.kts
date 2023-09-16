f667980d0927acb238e0d341797c4e4404dd9d5f
Nam Tran Trong <nam.tran-trong@ubisoft.com>
2023-09-16 22:05:42 +0700

    implement authentication with google

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -12,7 +13,6 @@ android {
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -33,7 +33,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
--- app/src/main/java/com/pbl/app_mobile/model/DO/LoginDO.java
+++ app/src/main/java/com/pbl/app_mobile/model/DO/LoginDO.java
@@ -1,5 +1,17 @@
 package com.pbl.app_mobile.model.DO;
+import android.app.Activity;
+import android.content.Intent;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.pbl.app_mobile.controller.LoginController;
 import com.pbl.app_mobile.model.BEAN.User;
 import com.pbl.app_mobile.utils.EmailValidator;
@@ -7,7 +19,8 @@ import com.pbl.app_mobile.utils.EmailValidator;
 public class LoginDO {
     private LoginController loginController;
     private boolean isPasswordHide = false;
-
+    private GoogleSignInOptions gso;
+    private GoogleSignInClient gsc;
     public void validateCredentials(User user){
         String errorText = "";
         if (user.getEmail().isEmpty()) {
@@ -19,11 +32,36 @@ public class LoginDO {
         }
         loginController.showValidationError(errorText);
     }
-    public LoginDO(LoginController loginController){
+    public LoginDO(LoginController loginController, Activity activity){
         this.loginController = loginController;
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(activity,gso);
     }
     public void changeStatusPassword(){
         isPasswordHide = !isPasswordHide;
         loginController.showHidePassword(isPasswordHide);
+    }
+    public void signInWithGoogle(int requestCode, Intent data){
+        if (requestCode == 1000){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                loginController.messageLoginWithGoogle(account.getDisplayName().toString() + "\n" + account.getEmail());
+            } catch (ApiException e) {
+                loginController.messageLoginWithGoogle("something went wrong");
+            }
+        }
+    }
+    public void signInWithGoogle(){
+        loginController.signInWithGoogle(gsc);
+    }
+    public void signOutWithGoogle(Activity activity){
+        gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(Task<Void> task) {
+                activity.finish();
+                loginController.messageLoginWithGoogle("signed out");
+            }
+        });
     }
 }
