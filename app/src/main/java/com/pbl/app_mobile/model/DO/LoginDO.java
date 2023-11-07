f3c1cbf4670befff4ad4c1fefd9651316264dda4
MinhQuan-Github <minhquan.5322@aol.com>
2023-11-07 18:53:17 +0700

    fix Network

--- app/src/main/java/com/pbl/app_mobile/model/DO/LoginDO.java
+++ app/src/main/java/com/pbl/app_mobile/model/DO/LoginDO.java
@@ -1,6 +1,7 @@
 package com.pbl.app_mobile.model.DO;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -21,6 +22,8 @@ import com.pbl.app_mobile.network.ApiManager;
 import com.pbl.app_mobile.network.ApiService;
 import com.pbl.app_mobile.network.JsonHandle;
 import com.pbl.app_mobile.utils.EmailValidator;
+
+import java.util.Objects;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -134,20 +137,20 @@ public class LoginDO {
                             loginController.navigateToHome();
                         }
                     } catch (Exception e) {
-
+                        Log.e("error", Objects.requireNonNull(e.getLocalizedMessage()));
                     }
                 } else {
                     try {
                         loginController.showValidationError(JsonHandle.getMessage(response, true));
                     } catch (Exception e) {
-
+                        Log.e("error", Objects.requireNonNull(e.getLocalizedMessage()));
                     }
                 }
             }
 
             @Override
             public void onFailure(Call<ResponseBody> call, Throwable t) {
-
+                Log.e("error", Objects.requireNonNull(t.getLocalizedMessage()));
             }
         });
     }
